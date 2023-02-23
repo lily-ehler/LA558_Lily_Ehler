4d23632db9fe4aac0f63f3419780ada46f9457ae
lily-ehler <lkehler@iastate.edu>
2023-02-23 14:15:22 -0600

    ex4a

---
+++ Exercises/4a/ex4a.R
@@ -0,0 +1,16 @@
+# Lily Ehler
+# L A 558
+# Exercise 4a
+
+# Plot 1 data: High temps in Ames 2/8 - 2/14
+ames_highs <- c(48, 37, 34, 48, 46, 55, 48)
+ames_date <- c(8, 9, 10, 11, 12, 13, 14)
+
+plot(ames_date, ames_highs)
+
+
+# Plot 2 data: Low temps in Ames 2/8 - 2/14
+ames_lows <- c(36, 19, 12, 25, 23, 27, 30)
+ames_dates <- c(8, 9, 10, 11, 12, 13, 14)
+
+plot(ames_date, ames_lows)
